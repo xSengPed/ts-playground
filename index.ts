01038126cf3c06f4c100a0234b5412035a802d24
Donnukrit S <kalima007c@gmail.com>
2023-11-04 18:47:11 +0700

    Initial Commit :sparkles:

---
+++ index.ts
@@ -0,0 +1,8 @@
+let fruits : String[] = ["Apple" , "Banana"]
+const pi : Number = 3.1415
+
+console.log("Hello World!")
+console.log("fruits -> " , fruits)
+console.log("pi -> ",pi)
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Readme
+
+``` $ npm install ``` 
+
+``` $ npm start```
